97701d1548164864cee80186a7a94fb3e45ccf1c
Agsvit <goncalovit@gmail.com>
2021-10-28 09:25:44 +0100

    initial

---
+++ weathermongo/src/test/java/com/bootcamp/weathermongo/WeathermongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.weathermongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeathermongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ weathersql/src/test/java/com/bootcamp/weathersql/WeathersqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.weathersql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeathersqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
